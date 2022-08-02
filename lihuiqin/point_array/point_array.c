ac7e569654cef3a50f11b32203ebe80e5a14de27
liuqi2021 <liuqi19940604@163.com>
2022-08-02 22:32:02 +0800

    [PORTING]Modified lihuiqin/point_array/point_array.c

--- lihuiqin/point_array/point_array.c
+++ lihuiqin/point_array/point_array.c
@@ -11,6 +11,6 @@ int main(){
     printf("array[1] = %d\n", (*p)[1]);
     printf("array[2] = %d\n", *(*p+2));
     printf("array[1] = %d\n", *(q+1));
-    printf("array[1] = %d\n", (*q)[1]);
+    printf("array[1] = %d\n", q[1]);
     exit(0);    
 }
