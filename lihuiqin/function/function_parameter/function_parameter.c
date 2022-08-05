afb7291915bd707b10f10f3bb4055ac0180b0795
liuqi2021 <liuqi19940604@163.com>
2022-08-05 22:26:49 +0800

    [Enhance]练习函数传参的方法

--- lihuiqin/function/function_parameter/function_parameter.c
+++ lihuiqin/function/function_parameter/function_parameter.c
@@ -1,3 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+int main(int argc, char *argv[]){
+    int i = 0;
+    char *s = "hello";
+    char s1[] = "ssss";
+    printf("s1 = %s\n", s1);
+    printf("s = %s\n", s);
+    puts(s1);
+    while(argv[i]){
+        // puts(argv[i]);
+        printf("%s\n", argv[i]);
+        printf("%p\n", argv[i]);
+        i++;
+    }
+
+}
